5d9012fd6ecd635e8e2006d5efb9160939c92025
Dezolder <evgeniy.zhukov.2012@gmail.com>
2022-07-06 01:02:23 +0300

    06 Обработка событий

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,20 +1,27 @@
 import React from "react";
 
 const Counter = () => {
-  const count = 0
+  let count = 0
   const formatCount = () => {
     return count === 0 ? 'emty' : count
   }
-  const getBageClasses = ()=>{
+  const getBageClasses = () => {
 
     let classes = 'badge m-2 '
-    classes += count === 0 ? 'bg-warning': 'bg-primary'
-  return classes
+    classes += count === 0 ? 'bg-warning' : 'bg-primary'
+    return classes
   }
+
+  const handleIncrement = () => {
+count++
+console.log(count)
+  }
+
   return (
     <>
       <span className={getBageClasses()}>{formatCount()}</span>
-      <button className="btn btn-primary btn-sm m-2">+</button>
+      <button className="btn btn-primary btn-sm m-2"
+        onClick={handleIncrement}>+</button>
     </>
   )
 };
